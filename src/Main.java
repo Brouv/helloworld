c664b9aa539e6b29eba24bf5e59c150c1784fc9a
testr <testrest136@gmail.com>
2024-06-01 19:28:59 +0300

    Massage changed to "Привет Мир!"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,69 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
-        System.out.println("Hello World!");
+
+//                 task#1
+
+         var dog = 8.0;
+         System.out.println(dog);
+         var cat = 3.6;
+         System.out.println(cat);
+         var paper = 763789;
+         System.out.println(paper);
+
+//                  task#2
+
+         var dog1 = dog + 4;
+         System.out.println(dog1);
+         var cat1 = cat + 4;
+         System.out.println(cat1);
+         var paper1 = paper + 4;
+         System.out.println(paper1);
+
+//                  task#3
+
+         var dog2 = dog1 - 3.5;
+         System.out.println(dog2);
+         var cat2 = cat1 - 1.6;
+         System.out.println(cat2);
+         var paper2 = paper1 - 7639;
+         System.out.println(paper2);
+
+//                  task#4
+
+         var friend = 19;
+         System.out.println(friend);
+         var friend1 = friend + 2;
+         System.out.println(friend1);
+         var friend2 = friend1 / 7;
+         System.out.println(friend2);
+
+//                  task#5
+
+         var frog = 3.5;
+         System.out.println(frog);
+         var frog1 = frog * 10;
+         System.out.println(frog1);
+         var frog2 = frog1 / 3.5;
+         System.out.println(frog2);
+         var frog3 = frog2 + 4;
+         System.out.println(frog3);
+
+//                   task#6
+
+         var boxer1 = 78.2;
+         System.out.println(boxer1 + " kg");
+         var boxer2 = 82.7;
+         System.out.println(boxer2 + " kg");
+         var totalWeight = boxer1 + boxer2;
+         System.out.println(totalWeight + "kg");
+         var difference = boxer2 - boxer1;
+         System.out.println("Разница между двумя боксёрами составляет " + difference + " kg");
+
+//                   task#7
+
+         var separationOfWeightsBoxers = boxer1 % boxer2;
+         System.out.println(separationOfWeightsBoxers);
+
+
     }
 }
