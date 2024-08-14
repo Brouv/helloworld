0ba95050f4b2f6ee6d0e1f669eff674ce599924c
testr <testrest136@gmail.com>
2024-08-14 11:56:06 +0300

    homework#9

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,53 @@
+import javax.swing.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        // task #1
+            //task #1
 
-        int money = 15000;
-        int total = 0;
-        int i = 0;
-        while (total < 2_459_000) {
-            total = total + money;
-            i++;
+        int[] sumCost = {30, 25, 29, 32, 33};
+        int sum = 0;
+        for (int element : sumCost) {
+            sum = sum + element;
         }
-        System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей.");
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
 
-        // task #2
+            //task #2
 
-        int a = 0;
-        while (a < 10) {
-            a++;
-            System.out.print(a);
-        }
-        System.out.println();
-        for (int b = 10; b > 0; b--) {
-            System.out.print(b);
-        }
-        System.out.println();
-
-        // task #3
-
-        int countryPopulation = 12_000_000;
-        int birthRate = countryPopulation / 1000 * 17;
-        int deathRate = countryPopulation / 1000 * 8;
-        int c = 0;
-        while (c < 10) {
-            c++;
-            countryPopulation = countryPopulation + birthRate - deathRate;
-            System.out.println("Год " + c + " численность населения составляет " + countryPopulation + ".");
-        }
-
-        // task #4
-
-        int contribution = 15000;
-        int d = 0;
-        while (contribution < 12_000_000) {
-            int percent = contribution / 100 * 7;
-            contribution = contribution + percent;
-            d++;
-            System.out.println("Месяц " + d + ", сумма по вкладу = " + contribution + " рублей.");
-        }
-
-        // task #5
-
-        int contribution1 = 15000;
-        int d1 = 0;
-        while (contribution1 < 12_000_000) {
-            int percent1 = contribution1 / 100 * 7;
-            contribution1 = contribution1 + percent1;
-            d1++;
-            if (d1 % 6 == 0) {
-                System.out.println("Месяц " + d1 + ", сумма по вкладу = " + contribution1 + " рублей.");
+        int[] cost = {30, 25, 29, 32, 33};
+        int minCost = 40;
+        for (final int current : cost) {
+        if (current < minCost){
+                minCost = current;
             }
         }
+       System.out.println("Минимальная сумма трат за неделю составила " + minCost + " рублей.");
 
-        // task #6
-
-        int contribution2 = 15000;
-        int d2 = 0;
-        while (d2 < 108) {
-            int percent2 = contribution2 / 100 * 7;
-            contribution2 = contribution2 + percent2;
-            d2++;
-            if (d2 % 6 == 0){
-            System.out.println("Месяц " + d2 + ", сумма по вкладу = " + contribution2 + " рублей.");
+        int maxCost = -1;
+        for (final int current : cost) {
+            if (current > maxCost) {
+                maxCost = current;
             }
         }
+        System.out.println("Максимальная сумма трат за неделю составила " + maxCost + " рублей.");
 
-            // task #7
+            //task #3
 
-        int friday = 5;
-        for (int currentFriday = friday; currentFriday < 31; currentFriday +=7){
-            System.out.println("Сегодня пятница, " + currentFriday + "-е число. Необходимо подготовить отчёт.");
+    int[] midCost = {30,43,44,35,37};
+    int sumMonth = 0;
+    for (int element : midCost){
+        sumMonth = sumMonth + element;
+    }
+        float midSum = sumMonth / 4.0f;
+        System.out.println("Средняя сумма трат за месяц составила " + midSum + " рублей");
+
+            //task #4
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
 
-            // task #8
-        int year = 2024;
-        int startYear = year - 200;
-        int stopYear = year + 100;
-        for (int e = startYear; e < stopYear; ++e){
-           if (e % 79 == 0){
-            System.out.println(e);
-            }
-        }
     }
 }
